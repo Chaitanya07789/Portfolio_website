ffb0cc156ef7528f10b78f06232dd2fea1a3f4ba
Chaitanya07789 <chaitanyalondhe730@gmail.com>
2025-08-25 23:44:05 +0530

    information_updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function tggleMean(){
+function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
